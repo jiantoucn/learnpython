f5c60d65e7daea7941bb99dc691095e945b6809f
jiantoucn <1048464071@qq.com>
2017-08-14 23:44:30 +0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -6,7 +6,3 @@ print 'Yay! Printing.' ,
 print "I'd much rather you 'not'." ,
 print 'I "said" do not touch this.',
 print "i love you.",
-zyx="zhuyuxuan"
-cx="chenxiao"
-print zyx "love" cx
-print cx "love" zyx
