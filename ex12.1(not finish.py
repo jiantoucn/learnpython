f6207c311efcd507e45aacf46cf837c15128d464
jiantoucn <1048464071@qq.com>
2017-08-16 01:23:49 +0800

    Add files via upload

---
+++ ex12.1(not finish.py
@@ -0,0 +1,3 @@
+print "do you love me?",
+love= raw_input()
+print "you %r me"%(love)
---
+++ ex14.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script,first,second,third = argv
+first=raw_input("can you write down first?")
+second=raw_input("can you write down second?")
+third=raw_input("can you write down third?")
+print "the script is called:", script
+print "your first variable is:", first
+print "your second variable is:", second
+print "your third variable is:", third
