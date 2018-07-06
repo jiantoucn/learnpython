39cb996928afc1bbdea1f3792b7dd0494c9a65f0
jiantoucn <1048464071@qq.com>
2018-07-06 22:03:23 +0800

    Add files via upload

--- list_use.py
+++ list_use.py
@@ -11,14 +11,15 @@ names = ['yuxuan','xiaoran','yiyan']
 message = "I Love " + names[0] + " But I Am Really bad,so I Love " + names[1] + " And "  + names[2] + " Too."
 print (message.title())
 #十分心痛T T
-
 names[2] = 'jingjing'#更换names列表中第三个元素
 #print ("but i think " + names[2] + " is very good ".title())
 message = "but i think " + names[2] + " is very good "
 print (message.title())
 #我永远喜欢萱萱 其他的都可以没有。。。
 #接下来是在列表末尾/在列表中间增加元素
-names.append("yiyan")#在尾部增加yiyan 
+names.append("yiyan")#在尾部增加yiyan 语法为 列表名.append("要在末尾添加的元素")
 print (names)#通过打印可以对比出和之前有什么区别
-names.insert(1,"yuxin")#重要 在这边单双引号无所谓！几乎可以的都这么认为 特殊情况暂时不考虑 这一条是指加入yuxin这个元素作为第二位 其他的往后类推
+names.insert(1,"yuxin")#重要 在这边单双引号无所谓！几乎可以的都这么认为 特殊情况暂时不考虑 这一条是指加入yuxin这个元素作为第二位 其他的往后类推    语法为列表名.insert(顺序号,"要添加的元素")
+print (names)#打印出即可看出不同
+del names[1]#删除列表中的某一个元素 语法是del 列表名[元素序号]
 print (names)#打印出即可看出不同
